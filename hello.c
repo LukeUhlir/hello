41aba859726189e9dc30d62b1b5e449220a9a28a
Luke Uhlir <luhlir4@huskers.unl.edu>
2024-08-29 11:51:50 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,18 @@
+/**
+ * Author: Luke Uhlir
+ *         luhlir4@unl.edu
+ * Date: 2024/08/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(int argc, char **argv) {
+
+  printf("Luke Uhlir!\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
